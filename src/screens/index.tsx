f852cbcb0786b5684aedd6d220ddfab4a5081192
Victor Caceres <caceres.vc95@gmail.com>
2020-10-21 16:15:37 -0400

    set theme color in safe area context

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import Main from './Main';
+import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native';
+import { DefaultProps } from 'src/types';
+import { connect } from 'react-redux';
+import Main from './Main';
 
-const Root = () => (
+const Root: FC<DefaultProps> = ({ theming: { theme } }) => (
   <NavigationContainer>
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.background }}>
       <Main/>
     </SafeAreaView>
   </NavigationContainer>
 );
 
-export default Root;
+const mapStateToProps = ({ theming }: DefaultProps): DefaultProps => ({ theming });
+
+export default connect(mapStateToProps)(Root);
