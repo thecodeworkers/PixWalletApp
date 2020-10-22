a6e4e1113820d4e39e6ba08a29b56e1ec39c10f0
Victor Caceres <caceres.vc95@gmail.com>
2020-10-22 12:47:02 -0400

    add transactions components and order navigations flow

--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Main, Summary } from './components';
+import { Main, Summary, CurrencyList, CurrencyActivity } from './components';
 import { HeaderNavigation } from '../../components';
 import Receive from '../Receive';
+import ReceiveFiat from '../ReceiveFiat';
+import ReceiveCrypto from '../ReceiveCrypto';
+import SendFiat from '../SendFiat';
+import SendCrypto from '../SendCrypto';
+import InternalTransaction from '../InternalTransaction';
 
 const Stack = createStackNavigator();
 
@@ -28,9 +33,37 @@ const Portfolio = () => {
           title: 'summary'
         }}
       />
-      <Stack.Screen
+      {/* <Stack.Screen
         name="receive"
         component={Receive}
+      /> */}
+      <Stack.Screen
+        name="currencyList"
+        component={CurrencyList}
+      />
+      <Stack.Screen
+        name="currencyActivity"
+        component={CurrencyActivity}
+      />
+      <Stack.Screen
+        name="receiveFiat"
+        component={ReceiveFiat}
+      />
+      <Stack.Screen
+        name="receiveCrypto"
+        component={ReceiveCrypto}
+      />
+      <Stack.Screen
+        name="sendFiat"
+        component={SendFiat}
+      />
+      <Stack.Screen
+        name="sendCrypto"
+        component={SendCrypto}
+      />
+      <Stack.Screen
+        name="internalTransaction"
+        component={InternalTransaction}
       />
     </Stack.Navigator>
   )
