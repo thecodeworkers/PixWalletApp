d138f3d398aa48d87cf9eea3496ca6a54daaf0f9
Victor Caceres <caceres.vc95@gmail.com>
2020-10-21 20:46:45 -0400

    remove persist config temporarily

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,27 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+// import AsyncStorage from '@react-native-community/async-storage';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import rootSaga from './sagas';
 
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage,
+  // storage: AsyncStorage,
   blacklist: [
     'theming'
   ]
 }
 
-const persistedReducer = persistReducer(persistConfig, reducers);
+// const persistedReducer = persistReducer(persistConfig, reducers);
 
 const sagaMiddleware = createSagaMiddleware();
-const store: any = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+const store: any = createStore(reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
 
-export { store, persistor };
+export {
+  store,
+  // persistor
+};
