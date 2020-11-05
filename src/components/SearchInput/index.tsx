83c9b86d0b2e79029567566aa91b5f355b3d0826
Victor Caceres <caceres.vc95@gmail.com>
2020-11-05 12:49:29 -0400

    whitelist layout first part

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -4,21 +4,21 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { View, TextInput } from 'react-native';
 import { Theming } from '../../types';
 
-const SearchInput: FC<Theming> = ({theme}) => {
+const SearchInput: FC<Theming> = ({ theme }) => {
   return (
     <View style={styles.inputParent}>
-    <TextInput
-      style={[styles.input, { borderColor: theme.defaultActiveIcon }]}
-      placeholder='Search'
-      placeholderTextColor={theme.veryLightGrey}
-    />
-    <Icon
-      name='search'
-      color={theme.veryLightGrey}
-      size={26}
-      style={styles.icon}
-    />
-  </View>
+      <TextInput
+        style={[styles.input, { borderColor: theme.defaultActiveIcon }]}
+        placeholder='Search'
+        placeholderTextColor={theme.veryLightGrey}
+      />
+      <Icon
+        name='search'
+        color={theme.veryLightGrey}
+        size={26}
+        style={styles.icon}
+      />
+    </View>
   )
 };
 
--- src/screens/Whitelist/Main/components/WhitelistItem/index.tsx
+++ src/screens/Whitelist/Main/components/WhitelistItem/index.tsx
@@ -1,22 +1,36 @@
 import React, { FC } from 'react';
-import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Separator } from '../../../../../components';
+import { BankIcon } from '../../../../../assets/image/svg/icons';
+import { generalShadow } from '../../../../../assets/styles';
+import { WhitelistItemProps } from './types';
 // import styles from './styles';
 
-const WhitelistItem: FC<any> = () => {
+const WhitelistItem: FC<WhitelistItemProps> = ({ theme, index }) => {
+
   return (
     <>
-      <TouchableOpacity style={{ height: 50, flexDirection: 'row', marginHorizontal: '5%', marginVertical: '6%' }}>
-        <View style={{ flex: 0.25 }}>
-
+      {
+        index == 0 ? (
+          <View style={{ marginTop: '10%' }}>
+            <Separator color={theme.whirspersInactive} />
+          </View>
+        ) : null
+      }
+      <TouchableOpacity style={{ height: 50, flexDirection: 'row', marginHorizontal: '5%', marginVertical: '6%' }} onPress={() => console.log(index)}>
+        <View style={{ flex: 0.25, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ width: 50, height: 50, justifyContent: 'center', alignItems: 'center', ...generalShadow() }}>
+            <View style={{ width: 30, height: 30 }}>
+              <BankIcon/>
+            </View>
+          </View>
         </View>
         <View style={{ flex: 0.7, justifyContent: 'center' }}>
-          <Text>Gabriel</Text>
-          <Text>1qwteydhfa132gswrdgsfqtt...</Text>
+          <Text style={{ color: theme.screenText, fontSize: 13, marginBottom: 2 }}>Gabriel</Text>
+          <Text style={{ color: theme.veryLightGrey, fontSize: 14 }}>1qwteydhfa132gswrdgsfqtt...</Text>
         </View>
       </TouchableOpacity>
-      <Separator/>
+      <Separator color={theme.whirspersInactive} />
     </>
   );
 }
